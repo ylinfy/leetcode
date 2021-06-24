81164c30a5e27c230cb63b4a4631ae840c4072cb
yang <yangluo1024@163.com>
2021-06-24 09:09:50 +0800

    clear

--- 2021/0001_twoSum/210622/twoSum.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashTable = dict()
-        for i, n in enumerate(nums):
-            if target - n in hashTable:
-                return [hashTable[target - n], i]
-            hashTable[n] = i
-        return []
