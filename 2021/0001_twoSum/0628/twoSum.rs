bd36bbc2c2254e00c2fc30f6e0ea00c2fc2716a5
yang <yangluo1024@163.com>
2021-06-28 07:18:41 +0800

    0628

---
+++ 2021/0001_twoSum/0628/twoSum.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        use std::collections::HashMap;
+
+        let len = nums.len();
+        let mut map = HashMap::with_capacity(len);
+        for i in 0..len {
+            if let Some(k) = map.get(&(target - nums[i])) {
+                return vec![*k as i32, i as i32];
+            }
+            map.insert(nums[i], i);
+        }
+        vec![]
+    }
+}
