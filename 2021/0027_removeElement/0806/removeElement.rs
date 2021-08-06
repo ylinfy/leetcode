fca4c4b15506047e42a464611ab1c78cbc4068d8
yang <yangluo1024@163.com>
2021-08-06 19:31:01 +0800

    0806

---
+++ 2021/0027_removeElement/0806/removeElement.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+        // nums.retain(|&x| x != val);
+        // nums.len() as i32
+        let mut slow = 0;
+        for fast in 0..nums.len() {
+            if nums[fast] == val {
+                continue;
+            }
+            nums[slow] = nums[fast];
+            slow += 1;
+        }
+        slow as i32
+    }
+}
